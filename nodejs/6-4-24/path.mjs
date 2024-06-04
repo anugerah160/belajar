fb3b37fbf1fcb4268d279b3395c61ae132edd63a
anugerah <157374618+anugerah160@users.noreply.github.com>
2024-06-04 16:08:36 +0700

    "changedir"

---
+++ nodejs/6-4-24/path.mjs
@@ -0,0 +1,6 @@
+import path from "path";
+const file = "Users/Administrator/Desktop/zona.txt";
+
+console.info(path.dirname(file));
+console.info(path.basename(file));
+console.info(path.extname(file));
